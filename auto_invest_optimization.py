7ed40f3a8dece76470f68a4ab1d7c77c1c7ac28a
now-eat-krap <916ss@naver.com>
2024-07-20 23:00:54 +0900

    코드 시작시 한번만 데이터 로드 하도록 수정

--- auto_invest_optimization.py
+++ auto_invest_optimization.py
@@ -28,21 +28,38 @@ exchange = ccxt.bybit(config={
     'enableRateLimit': True,
 })
 
-def now_ohlcv():
+def data_setting():
     df = pd.DataFrame()
-    ohlcv = exchange.fetch_ohlcv(symbols, '1m', limit=1000)
+    ohlcv = exchange.fetch_ohlcv(symbols, '5m', limit=1000)
     now_ohlcv = exchange.fetch_ticker(symbols)
 
     time = [datetime.fromtimestamp(ohlcv[i][0]/1000) for i in range(len(ohlcv))]
-    #+ timedelta(hours=9)
-    #time[-1] = datetime.strptime(now_ohlcv['datetime'], '%Y-%m-%dT%H:%M:%S.%fZ')+ timedelta(hours=9)
+
+    # setting ohlc
     df['time'] = time
     df['open'] = np.array(ohlcv).T[1]
     df['high'] = np.array(ohlcv).T[2]
     df['low'] = np.array(ohlcv).T[3]
     df['close'] = np.array(ohlcv).T[4]
-    df['ema'.format(1)] = df['close'].ewm(span=1, adjust=False).mean()
-    return df
+
+    # setting xATR, nLoss, ATRTrailingStop
+    SENSITIVITY = sensitivity
+    ATR_PERIOD = atr_period
+    df["xATR"] = ta.atr(df['high'],df['low'],df['close'], ATR_PERIOD)
+    df["nLoss"] = SENSITIVITY * df["xATR"]
+    df["ATRTrailingStop"] = [0.0] + [np.nan for i in range(len(df) - 1)]
+
+    for i in range(1, len(df)):
+        if df.loc[i, "close"] > df.loc[i - 1, "ATRTrailingStop"] and df.loc[i - 1, "close"] > df.loc[i - 1, "ATRTrailingStop"]:
+            df.loc[i, "ATRTrailingStop"] = max(df.loc[i - 1, "ATRTrailingStop"], df.loc[i, "close"] - df.loc[i, "nLoss"])
+        elif df.loc[i, "close"] < df.loc[i - 1, "ATRTrailingStop"] and df.loc[i - 1, "close"] < df.loc[i - 1, "ATRTrailingStop"]:
+            df.loc[i, "ATRTrailingStop"] = min(df.loc[i - 1, "ATRTrailingStop"], df.loc[i, "close"] + df.loc[i, "nLoss"])
+        elif df.loc[i, "close"] > df.loc[i - 1, "ATRTrailingStop"]:
+            df.loc[i, "ATRTrailingStop"] = df.loc[i, "close"] - df.loc[i, "nLoss"]
+        else:
+            df.loc[i, "ATRTrailingStop"] = df.loc[i, "close"] + df.loc[i, "nLoss"]
+
+    return df.iloc[-2]
 
 class Indicators:
     def ut_bot_alerts(self,df):
@@ -73,15 +90,7 @@ class Indicators:
 def main():
     exchange.load_markets()
     indicator = Indicators()
-    while True:
-    #if now.minute % 5 == 0:
-    #    time.sleep(1)
-        data_ = now_ohlcv()
-        #indicator.ut_bot_alerts(data_)
-
-        pprint.pprint(data_)
-        time.sleep(5)
-
+    print(data_setting())
 
 if __name__ == '__main__':
     main()
